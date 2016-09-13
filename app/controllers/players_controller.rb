1d1103272db0376dd293b235141c788884d7f51a
Anton <mezerny@gmail.com>
2016-09-13 22:50:43 +0300

    Fixed OPS calculation.

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -3,7 +3,7 @@ class PlayersController < ApplicationController
   def hitter_stats
     render json: Player.select('given_name, surname, home_runs, rbi, runs, steals')
                      .select('( hits / at_bats ::float ) as avg')
-                     .select('((hits + walks + hit_by_pitch) / (at_bats + walks + hit_by_pitch + sacrifice_flies) ::float) + ((hits + 2 * doubles + 3 * triples + 4 * home_runs) / at_bats ::float) as ops')
+                     .select('((hits + walks + hit_by_pitch) / (at_bats + walks + hit_by_pitch + sacrifice_flies) ::float) + ((hits +   doubles + 2 * triples + 3 * home_runs) / at_bats ::float) as ops')
                      .where.not(at_bats: nil)
                      .where.not(at_bats: 0)
                      .limit(params[:limit])
